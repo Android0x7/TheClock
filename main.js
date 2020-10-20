43b3215eea621f3f4248cdd5d5d2e00d5666db35
Ul Haque <umair.ul.haque@accenture.com>
2020-10-20 21:53:04 +0530

    Fullscreen

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function createWindow () {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 420,
-    fullscreen: false,
+    fullscreen: true,
     menuBarVisible: false,
     frame: false,
 
